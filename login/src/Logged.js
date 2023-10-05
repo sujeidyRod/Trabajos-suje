1f8af00dc0f9604bcccf3e896274eef66f5b3ef4
Suje <11458@utl.edu.mx>
2023-10-04 20:51:08 -0600

    escuela

--- login/src/Logged.js
+++ login/src/Logged.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import './App.css';
 
 function LogedIn({ username }) {
   return (
-    <div className="logged-in-container">
-      <div className="header">
-        <h1>Bienvenido {username}!</h1>
-        <p>Es un Gusto Tenerte Aquí.</p>
-      </div>
-      <div className="image-container">
-        {/* imagen */}
-        <img src="imagen1.jpg"/>
-      </div>
+    <div>
+      <h1>Bienvenido {username}!</h1>
+      <p>Es un Gusto Tenerte Aqui.</p>
     </div>
   );
 }
