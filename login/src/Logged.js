8fb96b3d32eb29adf7adeb688ee984833c96ea3f
Suje <11458@utl.edu.mx>
2023-10-04 21:30:40 -0600

    escuela

--- login/src/Logged.js
+++ login/src/Logged.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 function LogedIn({ username }) {
+  const containerStyle = {
+    backgroundColor: ' #BB8FCE',
+    padding: '20px',
+    textAlign: 'center',
+    borderRadius: '5px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+    width: '100vw', 
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  const textStyle = {
+    color: 'white',
+  };
+
   return (
-    <div>
-      <h1>Bienvenido {username}!</h1>
-      <p>Es un Gusto Tenerte Aqui.</p>
+    <div style={containerStyle}>
+      <h1 style={textStyle}>Bienvenido {username}!</h1>
+      <p style={textStyle}>Es un Gusto Tenerte Aquí.</p>
     </div>
   );
 }
 
 export default LogedIn;
+
+
